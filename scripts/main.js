a5d67c88069a271ad61fa200922b89c0d35f7857
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-02-20 12:56:55 +0600

    Load all the videos from API and use the Card Layout mentioned in Design part

--- scripts/main.js
+++ scripts/main.js
@@ -24,3 +24,46 @@ const displayCategories=(categories)=>{
 
 
 loadCategories();
+
+//create loadVideos
+
+const loadVideos=()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then( (res) => res.json())
+    .then((data)=>displayVideos(data.videos))
+    .catch((error)=>console.log(error))
+}
+
+//displayVideos
+
+const displayVideos=(videos)=>{
+    const videoContainer = document.getElementById('videos');
+    videos.forEach((video)=>{
+        console.log(video)
+        const card = document.createElement("div");
+        card.classList="card card-compact";
+        card.innerHTML=`
+         <figure class="h-[200px]">
+            <img class="h-full w-full object-cover "   
+            src=${video.thumbnail}
+            alt="Shoes" />
+          </figure>
+            <div class="px-0 py-2 flex gap-2">
+             <div>
+              <img
+              class="h-10 w-10 rounded-full object-cover  " 
+                src= ${video.authors[0].profile_picture}
+                alt="" />
+             </div>
+             <div>
+             <h3 class="font-bold text-lg ">${video.title}</h3>
+             </div>
+            
+        </div>
+        `
+        videoContainer.appendChild(card)
+        
+    })
+}
+
+loadVideos();
